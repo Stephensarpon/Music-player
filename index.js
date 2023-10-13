3e7befcb6f1cb1281708a91c9407110bef8c81fa
enochmakafui <boadu8617@gmai.com>
2023-10-13 08:59:03 +0000

    Complete music player

---
+++ index.js
@@ -0,0 +1,31 @@
+// $("h1").addClass("heading");
+// $("h1").removeClass("heading");
+
+// $("h1").text("World")
+
+// $("h1").css("color", "red");
+
+
+// $("a").attr("href", "https://mail.yahoo.com")
+
+// $("button").click(function(){
+//     $("h1").text("World")
+// })
+// let name = [];
+// $("input").keypress(function(event){
+//     const keyPress = event.key;
+//     name+= keyPress;
+    
+// })
+
+// $("button").click(function(){
+
+//     $("h1").text(`Helllo ${name}`)
+
+// })
+
+
+$("button").click(function(){
+    let song = new Audio("only.mp3");
+    song.play();
+})
